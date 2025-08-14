63b0c4a78fc824153db3ae33664ac64a550d50a6
김강민 <76114474+kimgm1018@users.noreply.github.com>
2025-08-15 00:22:13 +0900

    Update sangbot.py

--- sangbot.py
+++ sangbot.py
@@ -899,15 +899,15 @@ async def daily_report():
             post_channel = await bot.fetch_channel(chanel_id)
             await post_channel.send(result.content)
 
-@bot.command()
-async def 뉴스(ctx):
-    df = await get_yesterday_logs()  # 어제 채팅 로그 불러오기
-    if df is not None and not df.empty:
-        table_md = df.to_markdown(index=False)
-        result = sangchain.invoke({"log": table_md})
-        await ctx.send(result.content)  # 현재 명령어 친 채널로 전송
-    else:
-        await ctx.send("어제 기록이 없습니다.")
+# @bot.command()
+# async def 뉴스(ctx):
+#     df = await get_yesterday_logs()  # 어제 채팅 로그 불러오기
+#     if df is not None and not df.empty:
+#         table_md = df.to_markdown(index=False)
+#         result = sangchain.invoke({"log": table_md})
+#         await ctx.send(result.content)  # 현재 명령어 친 채널로 전송
+#     else:
+#         await ctx.send("어제 기록이 없습니다.")
 
 # 봇 준비되면 슬래시 명령어 서버에 등록
 @bot.event
